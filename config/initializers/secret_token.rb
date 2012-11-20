f05734b403e74271e1a19bf5e83d5bcf30e7b1ec
KukiNet <support@kukinet.com>
2012-11-20 12:09:27 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Menu::Application.config.secret_token = '872acc6c6115ec50f8d869838ffdda4a21359847713413895377b92ee2a9b29eef5ae398387b5f317ce9df8c74bff1356851a2c38f155312bc6d177df9567bb8'
